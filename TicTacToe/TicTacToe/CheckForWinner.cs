c4cdeb2e18827509a90f17cc31590f0fa53cb5e5
Sean <verrando19@gmail.com>
2021-09-27 23:53:56 +0900

    Update CheckForWinner.cs

--- TicTacToe/TicTacToe/CheckForWinner.cs
+++ TicTacToe/TicTacToe/CheckForWinner.cs
@@ -15,24 +15,24 @@ namespace TicTacToe
 			{
 				return 1;
 			}
-			//else if (position[1,0] == position[1,1] && position[1,1] == position[1,2])
-			//	return 1;
-			//else if (position[2,0] == position[2,1] && position[2,1] == position[2,2])
-			//	return 1;
-			//else if (position[0,0] == position[1,0] && position[1,0] == position[2,0])
-			//	return 1;
-			//else if (position[0,1] == position[1,1] && position[1,1] == position[2,1])
-			//	return 1;
-			//else if (position[0,2] == position[1,2] && position[1,2] == position[2,2])
-			//	return 1;
-			//else if (position[0,0] == position[1,1] && position[1,1] == position[2,2])
-			//	return 1;
-			//else if (position[0,2] == position[1,1] && position[1,1] == position[2,0])
-			//	return 1;
-			//else if (position[0,0] != '1' && position[0,1] != '2' && position[0,2] != '3' && position[1,0] != '4' &&
-			//	position[1,1] != '5' && position[1,2] != '6' && position[2,0] != '7' && position[2,1] != '8' && position[2,2] != '9')
-			//	return 0;
-			//else
+			else if (position[1,0] == position[1,1] && position[1,1] == position[1,2])
+				return 1;
+			else if (position[2,0] == position[2,1] && position[2,1] == position[2,2])
+				return 1;
+			else if (position[0,0] == position[1,0] && position[1,0] == position[2,0])
+				return 1;
+			else if (position[0,1] == position[1,1] && position[1,1] == position[2,1])
+				return 1;
+			else if (position[0,2] == position[1,2] && position[1,2] == position[2,2])
+				return 1;
+			else if (position[0,0] == position[1,1] && position[1,1] == position[2,2])
+				return 1;
+			else if (position[0,2] == position[1,1] && position[1,1] == position[2,0])
+				return 1;
+			else if (position[0,0] != '1' && position[0,1] != '2' && position[0,2] != '3' && position[1,0] != '4' &&
+				position[1,1] != '5' && position[1,2] != '6' && position[2,0] != '7' && position[2,1] != '8' && position[2,2] != '9')
+				return 0;
+			else
 				return -1;
 		}
     }
